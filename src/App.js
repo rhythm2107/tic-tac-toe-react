2572262f1cbf97305e01f054515291bd80666303
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-05-18 19:25:23 +0200

    Create a board with props and a Square component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-export default function Square() {
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice()
+    nextSquares[0] = "X"
+    setSquares(nextSquares)
+  }
+
   return (
     <>
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
+      </div>
     </>
   )
 }
